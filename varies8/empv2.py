f323020a16d47da51243d50b614fe114a132a79a
NaYoung, Kwon <8varies8@gmail.com>
2024-01-04 16:08:21 +0900

    사원등록 프로그램 v2 -- 상세조회, 수정, 삭제 추가

--- varies8/empv2.py
+++ varies8/empv2.py
@@ -1,26 +1,25 @@
 import sys
-emps = []
+import json
+import sys
+from collections import OrderedDict
 
+emps = {'response': {'body': {'totalCount': 0, 'items': []}}}
+items = []
 
 def load_employees():
     print('프로그램을 초기화합니다')
     global emps
-    dicts = []
+    global items
 
-    with open('employees.csv') as f:
-        datas = f.readlines()
+    try:                    # 만일 작업중 오류가 발생하면
+        with open('employees.json', encoding='UTF-8')as f:
+            emps = json.load(f)
+            items = emps['response']['body']['items']
+    except:
+        # 프로그램 실행 중단없이 다음코드 실행
+        items = emps['response']['body']['items']
 
-    for data in datas:
-        item = data.strip().split(',')
-        d = {'empno':item[0], 'fname':item[1], 'lname':item[2], 'email':item[3], 'hdate':item[4],
-            'jobid':item[5], 'sal':item[6], 'deptid':item[7]}
-        dicts.append(d)
-
-    emps = dicts
-print('초기화 완료')
-
-
-
+    print('초기화 완료')
 
 
 def show_menu():
@@ -57,9 +56,9 @@ def input_employee():
 def save_employee(emp):
     row = (f"{emp['empno']}, {emp['fname']}, {emp['lname']}, {emp['email']}, "
            f"{emp['hdate']}, {emp['jobid']}, {emp['sal']}, {emp['deptid']}\n")
-    with open('employees.csv', 'a') as f:
+    with open('employees.json', 'a') as f:
         f.write(row)
-    emps.append(emp)
+    items.append(emp)
 
 
 def add_employee():   #데이터 입력받음
@@ -72,7 +71,7 @@ def add_employee():   #데이터 입력받음
 def read_employee():
     print('모든 사원정보를 조회합니다')
     result = ''
-    for emp in emps:
+    for emp in items:
         result += (f"{emp['empno']}\t{emp['fname']}\t{emp['jobid']}\t{emp['deptid']}\n")
     print(result)
 
@@ -80,14 +79,79 @@ def read_employee():
 
 def readone_employee():
     print('특정사원의 상세정보를 조회합니다')
+    empno = input('상세조회할 사원번호는?')
+
+    info = ('찾는 데이터가 없습니다.')
+    for emp in items:
+        if emp['empno'] == empno:
+            info = (f"{emp['empno']}, {emp['fname']}, {emp['lname']}, {emp['email']}, "
+                    f"{emp['hdate']}, {emp['jobid']}, {emp['sal']}, {emp['deptid']}\n")
+            break
+
+    print(info)
+
+
+
+def read_again(data, empno):
+    emp = OrderedDict()
+    emp['fname'] = int(input(f'새 이름: (기존:{data["fname"]}'))
+    emp['email'] = int(input(f'새 이메일: (기존:{data["email"]}'))
+    emp['jobid'] = int(input(f'새 직책: (기존:{data["jobid"]}'))
+    emp['sal'] = int(input(f'새 급여: (기존:{data["sal"]}'))
+    emp['deptid'] = int(input(f'새 부서번호: (기존:{data["deptid"]}'))
+    return data
+
+
+
+def flush_employee():
+    with open('employees.json', 'w',) as f:
+        json.dump(emps, f, ensure_ascii=False)
 
 
 def modify_employee():
     print('특정사원의 정보를 수정합니다')
+    empno = input('수정할 사원의 사원번호는?')
+
+    data = None
+    idx = None
+    for i, emp in enumerate(items):
+        if emp['empno'] == empno:
+            data = emp
+            idx = i
+
+    if data:
+        data = read_again(data, empno)
+        items[idx] = data
+        flush_employee()
+
+    else:
+        print('찾는 데이터가 없습니다.')
+
+
+
 
 
 def remove_employee():
     print('특정사원의 정보를 제거합니다')
+    empno = input('삭제할 사원의 사번')
+
+    data = None
+    for emp in items:
+        if emp['empno'] == empno:
+            data = emp
+            break
+
+    if data:
+        confirm = input('정말 삭제하시겠습니까? (yes/no): ')
+        if confirm == 'yes':
+            items.remove(data)
+            emps['response']['body']['totalCount'] -= 1
+            print(f'{empno}의 데이터가 삭제되었습니다.')
+            flush_employee()
+        else:
+            print('삭제가 취소되었습니다.')
+
+
 
 
 def exit_program():
