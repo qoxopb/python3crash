acb05de698288dc6bf60319071e6d5d7696ba9fc
NaYoung, Kwon <8varies8@gmail.com>
2024-01-10 14:09:39 +0900

    도서관리 프로그램 v1 - insert_book 구현

---
+++ varies8/BookDAO.py
@@ -0,0 +1,45 @@
+import varies8.dbinfo2 as dbinfo
+
+insertsql = ' insert into book (bkname, author, pubilisher, pubdate, retail, '\
+             ' price, pctoff,mileage) values (%s,%s,%s,%s,%s,%s,%s,%s) '
+
+selectsql = ' select bkno, bkname, author, price from book '
+
+selectonesql = ' select * from book where bkno = %s '
+
+updatesql =  'update book set bkname = %s, author = %s, publisher = %s,  '\
+             ' pubdate = %s, retail = %s, pctoff = %s '\
+             ' where bkno = %s '
+
+deletesql = ' delete book wher bkno = %s '
+
+
+class BookDAO:
+    @staticmethod
+    def insert_book(bk):
+        cursor, conn = dbinfo.openConn()
+        params = [bk.bkname, bk.author, bk.pubilisher, bk.pubdate,
+                  int(bk.retail), int(bk.price), int(bk.pctoff), int(bk.milege)]
+
+        cursor.execute(insertsql, params)
+        conn.commit()
+        rowcnt = cursor.rowcount
+
+        dbinfo.closeConn(cursor, conn)
+        return rowcnt
+
+
+    @staticmethod
+    def select_book(self):
+        pass
+
+
+    @staticmethod
+    def update_book(self):
+        pass
+
+
+    @staticmethod
+    def delelte_book(self):
+        pass
+
