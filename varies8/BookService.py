18bd543243c2ca89b246938d46dd916d03c3fb21
NaYoung, Kwon <8varies8@gmail.com>
2024-01-10 12:16:43 +0900

    도서관리 프로그램v1 - new_book 구현 (input_book 포함)

--- varies8/BookService.py
+++ varies8/BookService.py
@@ -1,22 +1,6 @@
 import sys
-from varies8.SungJuk import SungJuk
-from varies8.SungJukService import SungJukservice
-from varies8.SungJukDAO import SungJukDAO
-
-
-# 성적 데이터 추가 (입력-처리-저장)
-def addsungjuk():
-    """
-    새로운 성적 데이터 추가 (입력 처리 저장)
-    :return: 없음
-    """
-    print('성적데이터 추가')
-    sj = SungJukservice.read_sungjuk()
-    SungJukservice.compute_sunjuk(sj)
-
-    rowcount = SungJukDAO.insert_sungjuk(sj)
-
-    print(f'{rowcount}건의 성적데이터가 추가됨.')
+from varies8.Book import Book
+from varies8.BookDAO import BookDAO
 
 
 
@@ -28,13 +12,13 @@ def show_menu():
     """
     main_menu = '''
     -----------------------
-    성적처리프로그램 v7
+    도서 관리 프로그램 V1
     -----------------------
-    1. 성적 데이터 추가
-    2. 성적 데이터 조회
-    3. 성적 데이터 상세조회
-    4. 성적 데이터 수정
-    5. 성적 데이터 삭제
+    1. 도서 데이터 추가
+    2. 도서 데이터 조회
+    3. 도서 데이터 상세조회
+    4. 도서 데이터 수정
+    5. 도서 데이터 삭제
     0. 프로그램 종료
     -----------------------
     '''
@@ -44,50 +28,58 @@ def show_menu():
 
 
 
+# 도서 데이터 추가 (입력-처리-저장)
+def input_book():
+    bkname = input('도서명 : ')
+    author = input('저자 : ')
+    pubilisher = input('출판사 : ')
+    pubdate = input('출간일 : ')
+    retail = input('정가 : ')
+    pctoff = input('할인율 : ')
 
-# 성적 데이터 저장(sungjuk 테이블)
-def save_sungjuk(sj):
+    bk = Book(bkname, author, pubilisher, pubdate, retail, pctoff)
+    bk.price = bk.retail * (1 - (bk.pctoff / 100))
+    bk.milege = bk.retail * (bk.pctoff / 100)
+
+    return bk
+
+
+def new_book():
     """
-    성적 데이터 저장(sungjuk 테이블)
-    :param sj: 입력받아 처리된 성적데이터(총점평균등급)
+    새로운 도서 데이터 추가 (입력 처리 저장)
     :return: 없음
     """
-    sql =' insert into sungjuk (name, kor, eng, mat, tot, avg, grd) ' \
-         ' values (:1,:2,:3,:4, :5,:6,:7) '
+    print('도서데이터 추가')
+    bk = input_book()
 
-    conn = oracledb.connect(
-        user=userid, password=passwd, dsn=dsn_tns)
-    cursor = conn.cursor()
-    cursor.execute(sql, sj)
-    conn.commit()
-    print(cursor.rowcount, '건의 성적데이터 추가됨.')
-
-    cursor.close()
-    conn.close()
+    rowcount = BookDAO.insert_book(bk)
+    print(f'{rowcount}건의 도서데이터가 추가됨.')
 
 
 
-# 모든 성적 데이터 출력(번호/이름/국/영/수/등록일)
-def show_sungjuk():
+
+
+# 모든 도서 데이터 출력
+def read_book():
     """
-    성적조회 - 모든 성적 데이터 출력(번호/이름/국/영/수/등록일)
+    도서조회 - 모든 도서 데이터 출력(번호/도서명/저자/출판사/판매가)
     :return: 없음
     """
-    print('성적데이터 조회')
-    rows = SungJukDAO.select_sungjuk()
+    print('도서데이터 조회')
+    rows = BookDAO.select_book()
     for row in rows:
-        print(f'{row[0]} {row[1]} {row[2]} {row[3]} {row[4]} {str(row[5])[:10]}')
+        print(f'{row[0]} {row[1][:10]} {row[2]} {row[3]} {str(row[4])}')
 
 
-#성적 상세조회
-def showone_sungjuk():
+#도서 상세조회
+def readone_book():
     """
-    성적조회 - 과목점수 + 총점평균등급
+    도서데이터 상세조회
     :return: 없음
     """
-    sjno = input('상세 조회할 학생 번호?:')
+    bkno = input('상세 조회할 도서명 : ')
 
-    row = SungJukDAO.selectone_sungjuk(sjno)
+    row = BookDAO.selectone_book(bkno)
 
     print(f'{row[0]} {row[1]} {row[2]} {row[3]} {row[4]} {row[5]} {row[6]} {row[7]} {row[8]}')
 
@@ -96,26 +88,26 @@ def showone_sungjuk():
 
 
 
-# 성적데이터 수정
-def modify_sungjuk():
+# 도서데이터 수정
+def modify_book():
     """
-    성적데이터 수정
+    도서데이터 수정
     :return:
     """
-    sjno = input('수정할 학생 번호: ')
+    bkno = input('수정할 도서 번호: ')
     # 튜플객체를 수정하기 위해 리스트객체로 변환
-    sj = list(SungJukDAO.selectone_sungjuk(sjno))
+    bk = list(BookDAO.selectone_book(bkno))
 
-    if sj[0]: # 만일, 수정데이터가 존재한다면
-        sj[1] = (input(f'새 이름: {sj[1]}) : '))
-        sj[2] = int(input(f'새 국어 점수: (기존점수:{sj[2]}) : '))
-        sj[3] = int(input(f'새 영어 점수: (기존점수:{sj[3]}) : '))
-        sj[4] = int(input(f'새 수학 점수: (기존점수:{sj[4]}) : '))
-        # 조회한 결과를 클래스 타입으로 변경 후 다시 성적처리
-        sj = SungJuk(sj[1], sj[2], sj[3], sj[4])
-        SungJukservice.compute_sunjuk(sj)
+    if bk[0]: # 만일, 수정데이터가 존재한다면
+        bk[1] = (input(f'새 이름: {bk[1]}) : '))
+        bk[2] = int(input(f'새 국어 점수: (기존점수:{bk[2]}) : '))
+        bk[3] = int(input(f'새 영어 점수: (기존점수:{bk[3]}) : '))
+        bk[4] = int(input(f'새 수학 점수: (기존점수:{bk[4]}) : '))
+        # 조회한 결과를 클래스 타입으로 변경 후 다시 도서처리
+        bk = Book(bk[1], bk[2], bk[3], bk[4])
+        BookService.compute_sunjuk(bk)
 
-        rowcnt = SungJukDAO.update_sungjuk(sj, sjno)
+        rowcnt = BookDAO.update_book(bk, bkno)
         print(f'{rowcnt} 건의 데이터가 수정되었습니다.')
 
 
@@ -124,23 +116,23 @@ def modify_sungjuk():
 
 
 
-# 성적데이터 삭제
-def remove_sungjuk():
+# 도서데이터 삭제
+def remove_book():
     """
-    성적데이터 삭제
+    도서데이터 삭제
     :return:
     """
-    sjno = input('삭제할 학생 번호: ')
-    rowcnt = SungJukDAO.delete_sungjuk(sjno)
+    bkno = input('삭제할 도서 번호: ')
+    rowcnt = BookDAO.delete_book(bkno)
     print(f'{rowcnt} 건의 데이터가 삭제되었습니다.')
 
 
 
 
-# 성적프로그램 종료
+# 도서프로그램 종료
 def exit_program():
     """
-    성적처리 프로그램 종료 함수
+    도서처리 프로그램 종료 함수
     :param: 없음
     :return: 없음
     """
